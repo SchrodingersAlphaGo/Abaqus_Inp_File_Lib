a8051bab8a8cf0fdb76078db7688ecb1dbaa92cf
SchrodingersAlphaGo <zhanganyong@tju.edu.cn>
2023-02-15 19:46:51 +0800

    v

--- test.py
+++ test.py
@@ -92,18 +92,69 @@ for setname, set in allNsets.items():
     kdtree.BuildLocatorFromPoints(vpts)
 
     # find first column points
-    firstColumnPoints = []
-    for i in set:
-        # find closest 4 points
-        pId = set[i]
-        p = allNodes[pId] 
+    pId = 0
+    firstColumnPoints = [pId]
+    for i in range(10):
+        if i >= len(firstColumnPoints):
+            break
+        p = vpts.GetPoint(pId)
         idList = vtk.vtkIdList()
         kdtree.FindClosestNPoints(5, p, idList)
+        zAxisPtsId = []
+        zAxisDz = []
+        xyAxisPtsId = []
+
         for j in range(idList.GetNumberOfIds()):
             pIdj = idList.GetId(j)
-            if pIdj == pId: continue
+            # print("pidj : ", pIdj)
+            if pIdj == pId: 
+                continue
             pj = vpts.GetPoint(pIdj)
-            
+            # print(pj[2] , p[2])
+            dz = pj[2] - p[2]
+            if np.abs(dz) < 5e-1:
+                xyAxisPtsId.append(pIdj)
+            else:
+                zAxisPtsId.append(pIdj)
+                zAxisDz.append(dz)
+            print(pId, pIdj, dz)
+        # print(xyAxisPtsId)
+        # print(zAxisPtsId)
+        # exclude z axial neighbor point at same side
+        rmPid = []
+        for m in range(len(zAxisDz)):
+            for n in range(m+1, len(zAxisDz)):
+                if zAxisDz[m] * zAxisDz[n] > 0:
+                    if np.abs(zAxisDz[m]) < np.abs(zAxisDz[n]):
+                        rmPid.append(zAxisPtsId[n])
+                    else:
+                        rmPid.append(zAxisPtsId[m])
+
+        # if len(zAxisDz) == 2 and zAxisDz[0] * zAxisDz[1] > 0:
+        #     if np.abs(zAxisDz[0]) < np.abs(zAxisDz[1]):
+        #         zAxisPtsId.pop(1)
+        #     else:
+        #         zAxisPtsId.pop(0)
+        # elif len(zAxisDz) == 3:
+        #     if zAxisDz[0] * zAxisDz[1] > 0:
+        #         if np.abs(zAxisDz[0]) < np.abs(zAxisDz[1]):
+        #             zAxisPtsId.pop(1)
+        #         else:
+        #             zAxisPtsId.pop(0)
+        for x in rmPid:
+            zAxisPtsId.remove(x)
+
+        for x in zAxisPtsId:
+            if x not in firstColumnPoints:
+                firstColumnPoints.append(x)
+        print("zids : ", zAxisPtsId)
+        print("1st col : ",firstColumnPoints)
+        pId = firstColumnPoints[i+1]
+        print("pId : ", pId)
+    # for id in firstColumnPoints:
+        # p = vpts.GetPoint(id)
+        # print(id, p[2])
+    exit()
 
 # 
 
